6407f63bb55cd33fa921b0a10b2c9da0672eb4cf
Tiago Fernandes Ribeiro <tiagofernandesribeiro@yahoo.com.br>
2023-05-05 19:59:46 -0300

    Model

---
+++ src/main/java/edu/senac/model/UserModel.java
@@ -0,0 +1,49 @@
+package edu.senac.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.br.CPF;
+
+@Entity
+@Table(name = "TB_USER")
+public class UserModel implements Serializable{
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotEmpty(message = "O nome é obrigatório!")
+    @Size(min=2, message="O nome deve, no mínimo, ter 3 caracteres")
+    @Column(name = "nome", length = 50, nullable = false)
+    private String nome;
+
+    @Email(message="Insira um e-mail válido!")
+    @NotEmpty(message = "O e-mail é obrigatório!")
+    @Column(name = "email", length = 80, nullable = false)
+    private String email;
+
+    @CPF
+    @NotEmpty(message = "O CPF é obrigatório!")
+    @Column(name = "cpf", length = 11)
+    private String cpf;
+
+    @Size(min=5, message="A senha deve, no mínimo, ter 5 caracteres")
+    @NotEmpty(message = "A senha é obrigatória!")
+    @Column(name = "senha", columnDefinition = "TEXT", nullable = false)
+    private String senha;
+
+    @NotEmpty(message = "A data de nascimento é obrigatória!")
+    @Column(name = "data_nasc", nullable = false)
+    private String telefone;
+}
