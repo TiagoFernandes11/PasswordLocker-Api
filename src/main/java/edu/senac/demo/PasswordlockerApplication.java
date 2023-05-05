d3611af9c2d087828d54a0a259d510bd45ddabe5
Tiago Fernandes Ribeiro <tiagofernandesribeiro@yahoo.com.br>
2023-05-05 19:33:32 -0300

    initializr

---
+++ src/main/java/edu/senac/demo/PasswordlockerApplication.java
@@ -0,0 +1,13 @@
+package edu.senac.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordlockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasswordlockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/senac/demo/PasswordlockerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.senac.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordlockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
