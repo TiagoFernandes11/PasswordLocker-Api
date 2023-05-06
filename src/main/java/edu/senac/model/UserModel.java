5b85d5eb32501a2983387161d196986edc654fc3
Tiago Fernandes Ribeiro <tiagofernandesribeiro@yahoo.com.br>
2023-05-05 22:04:07 -0300

    Varias parada

--- src/main/java/edu/senac/model/UserModel.java
+++ src/main/java/edu/senac/model/UserModel.java
@@ -12,7 +12,7 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
-import org.hibernate.validator.constraints.br.CPF;
+//import org.hibernate.validator.constraints.br.CPF;
 
 import lombok.Data;
 
