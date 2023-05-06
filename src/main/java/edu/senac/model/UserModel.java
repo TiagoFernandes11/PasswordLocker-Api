98a2ebf34b7e089a53ac2cea1e0eaa126aacf74c
NathaliadaRocha07 <nathaliarochasantos9@gmail.com>
2023-05-05 22:16:49 -0300

    Add Models senha e website

--- src/main/java/edu/senac/model/UserModel.java
+++ src/main/java/edu/senac/model/UserModel.java
@@ -12,7 +12,6 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
-import org.hibernate.validator.constraints.br.CPF;
 
 import lombok.Data;
 
